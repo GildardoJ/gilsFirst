e8c89004eaf8d225002dc7e225a324fbbd7cf1a1
Gildardo Orozco <gorozco@gildardos-mac.local>
2019-01-28 16:13:26 -0800

    Initial Commit

---
+++ gilsFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gilsFirst
+//
+//  Created by Gildardo Orozco on 1/28/19.
+//  Copyright © 2019 Gildardo Orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
