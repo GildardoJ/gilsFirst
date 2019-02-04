ab1aef80f53a92197771ac76fc5d957b60af189c
Gildardo Orozco <gorozco@Gildardos-Mac.local>
2019-02-04 13:36:19 -0800

    second commit

---
+++ gilsFirst/MovieViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieViewController.swift
+//  gilsFirst
+//
+//  Created by Gildardo Orozco on 1/28/19.
+//  Copyright © 2019 Gildardo Orozco. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: NSObject {
+
+}
