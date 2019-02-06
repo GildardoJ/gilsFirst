c32c0b32d07ca63b80184cc52b2b0a1595d9af31
Gildardo Orozco <gorozco@Gildardos-Mac.local>
2019-02-06 09:32:38 -0800

    finished, readme needed

--- gilsFirst/MoviesViewController.swift
+++ gilsFirst/MoviesViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import AlamofireImage
+ 
 class MoviesViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
@@ -67,6 +68,9 @@ class MoviesViewController: UIViewController , UITableViewDataSource, UITableVie
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
         return cell
         
     }
